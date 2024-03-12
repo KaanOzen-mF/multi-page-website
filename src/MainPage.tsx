b8d361688605ea4182be667222e22a9b5b7a8406
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-12 14:12:15 +0300

    Add Navbar component to MainPage

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,12 +1,8 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const MainPage: React.FC = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-jost underline text-red-300">Home Page</h1>
-      <p className="font-jost">Welcome to the home page</p>
-    </div>
-  );
+  return <Navbar />;
 };
 
 export default MainPage;
