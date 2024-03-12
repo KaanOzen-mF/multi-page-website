59541e8937e409eb9edadf1c882abfb83adb8593
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-12 16:33:55 +0300

    Update NavbarLogo styling and font size

--- src/components/Navbar/NavbarLogo.tsx
+++ src/components/Navbar/NavbarLogo.tsx
@@ -4,8 +4,8 @@ const NavbarLogo: React.FC = () => {
   return (
     <div className="flex">
       <div className="flex">
-        <img className="pr-2" src="/src/assets/Oval.png" alt="" />
-        <p className="font-jost font-bold tracking-widest">DESIGNO</p>
+        <img className="pr-2 w-full" src="/src/assets/Oval.png" alt="" />
+        <p className="font-jost font-bold text-2xl tracking-widest">DESIGNO</p>
       </div>
     </div>
   );
