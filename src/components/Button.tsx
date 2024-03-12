36d0566ebcd708aa4559f550665a2f7687227c60
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-12 14:12:32 +0300

    Update Button component styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, className }) => {
   return (
     <button
-      className={`font-jost rounded-md px-4 py-3 hover:bg-orange-300 hover:text-orange-50 ${className}`}
+      className={`font-jost rounded-md px-4 py-3 hover:bg-lightPeach hover:text-white ${className}`}
       onClick={() => console.log("Button clicked")}
     >
       {children}
