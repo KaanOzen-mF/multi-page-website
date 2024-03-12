a6f8ee495387896da647800f865fd15df1062457
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-12 14:11:58 +0300

    Add new colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ export default {
     fontFamily: {
       jost: ["Jost", "sans-serif"],
     },
+    colors: {
+      peach: "#e7816b",
+      black: "#1d1c1e",
+      white: "#ffffff",
+      lightPeach: "#ffad9b",
+      darkGrey: "#333136",
+      lightGrey: "#f1f3f5",
+    },
   },
   plugins: [],
 };
