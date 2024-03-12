c75713d023ca23e1cc92da802f38d2471b696b7a
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-12 14:12:56 +0300

    Add NavbarLogo component

---
+++ src/components/Navbar/NavbarLogo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavbarLogo: React.FC = () => {
+  return (
+    <div className="flex">
+      <div className="flex">
+        <img className="pr-2" src="/src/assets/Oval.png" alt="" />
+        <p className="font-jost font-bold tracking-widest">DESIGNO</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarLogo;
