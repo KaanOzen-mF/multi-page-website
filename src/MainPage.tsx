aaa58c636beb5c0f5f2408be39a988337f26e55e
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-12 12:07:29 +0300

    Add MainPage component

---
+++ src/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-jost underline text-red-300">Home Page</h1>
+      <p className="font-jost">Welcome to the home page</p>
+    </div>
+  );
+};
+
+export default MainPage;
