5c296186d41f6062c07fc387459cb5a88bdd4432
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-12 14:12:07 +0300

    Remove unused Button component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import Button from "./components/Button";
 import MainPage from "./MainPage";
 
 function App() {
   return (
     <>
       <MainPage />
-      <Button children={"LEARN MORE"} className="bg-orange-50" />
-      <Button
-        children={"LEARN MORE"}
-        className="bg-orange-500 text-orange-50"
-      />
     </>
   );
 }
