04b31b56ff7b0072974069361173b041ec06265c
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 16:50:09 +0300

    Add DesignCard components and useNavigate hook in GraphicDesign.tsx

--- src/components/GraphicDesignPage/GraphicDesign.tsx
+++ src/components/GraphicDesignPage/GraphicDesign.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import DesignPageHeader from "../DesignComponent/DesignPageHeader";
 import GraphicDesignPageCardContainer from "./GraphicDesignPageCardContainer";
+import DesignCard from "../DesignComponent/DesignCard";
+import { useNavigate } from "react-router-dom";
 
 const GraphicDesign: React.FC = () => {
+  const navigate = useNavigate(); // Initialize useHistory
   return (
     <div>
       <DesignPageHeader
@@ -10,6 +13,20 @@ const GraphicDesign: React.FC = () => {
         description="We deliver eye-catching branding materials that are tailored to meet your business objectives."
       />
       <GraphicDesignPageCardContainer />
+      <DesignCard
+        title="APP DESIGN"
+        backgroundImage="/src/assets/image-app-design.jpg"
+        onProjectClick={() => {
+          navigate("/web-design"); // Navigate to web design page
+        }}
+      />
+      <DesignCard
+        title="WEB DESIGN"
+        backgroundImage="/src/assets/image-web-design.jpg"
+        onProjectClick={() => {
+          navigate("/web-design"); // Navigate to web design page
+        }}
+      />
     </div>
   );
 };
