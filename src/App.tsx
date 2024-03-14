075c43501cd130757fc15d6b21ffdaaa151e2c4f
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 11:25:15 +0300

    Update imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import MainPage from "./MainPage";
-import AboutCardContainer from "./components/AboutCardContainer";
+import AboutCardContainer from "./components/AboutCardComponent/AboutCardContainer";
 import DesignCardContainer from "./components/DesignComponent/DesignCardContainer";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <MainPage />
       <DesignCardContainer />
       <AboutCardContainer />
+      <Footer />
     </>
   );
 }
