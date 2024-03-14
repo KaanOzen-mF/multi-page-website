92796739c048ae5003c543306f6ea230f43d3a40
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 17:05:52 +0300

    Add DesignCard components and useNavigate hook

--- src/components/AppDesignPage/AppDesign.tsx
+++ src/components/AppDesignPage/AppDesign.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import DesignPageHeader from "../DesignComponent/DesignPageHeader";
 import AppDesignPageCardContainer from "./AppDesignPageCardContainer";
+import DesignCard from "../DesignComponent/DesignCard";
+import { useNavigate } from "react-router";
 
 const AppDesign: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <>
       <DesignPageHeader
@@ -10,6 +13,20 @@ const AppDesign: React.FC = () => {
         description="Our mobile designs bring intuitive digital solutions to your customers right at their fingertips."
       />
       <AppDesignPageCardContainer />
+      <DesignCard
+        title="WEB DESIGN"
+        backgroundImage="/src/assets/image-app-design.jpg"
+        onProjectClick={() => {
+          navigate("/web-design"); // Navigate to web design page
+        }}
+      />
+      <DesignCard
+        title="GRAPHIC DESIGN"
+        backgroundImage="/src/assets/image-web-design.jpg"
+        onProjectClick={() => {
+          navigate("/graphic-design"); // Navigate to graphic design page
+        }}
+      />
     </>
   );
 };
