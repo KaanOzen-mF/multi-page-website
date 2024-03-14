efea6b413f93d1ed2d20a18f92d5241e487e00f3
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 16:50:23 +0300

    Update design page cards

--- src/components/GraphicDesignPage/GraphicDesignPageCardContainer.tsx
+++ src/components/GraphicDesignPage/GraphicDesignPageCardContainer.tsx
@@ -5,34 +5,21 @@ const GraphicDesignPageCardContainer: React.FC = () => {
   return (
     <div>
       <DesignPageCard
-        img="/src/assets/Express.png"
-        title="Exspress"
-        description="A multi-carrier shipping website for ecommerce businesses"
+        img="/src/assets/timbrown.png"
+        title="TIM BROWN"
+        description="A book cover designed for 
+        Tim Brown’s new release, ‘Change’"
+      />
+
+      <DesignPageCard
+        img="/src/assets/boxedwater.png"
+        title="boxed water"
+        description="A simple packaging concept made for Boxed Water"
       />
       <DesignPageCard
-        img="/src/assets/Transfer.png"
-        title="transfer"
-        description="Site for low-cost money transfers and sending money within seconds"
-      />
-      <DesignPageCard
-        img="/src/assets/Photon.png"
-        title="photon"
-        description="A state-of-the-art music player with high-resolution audio and DSP effects"
-      />
-      <DesignPageCard
-        img="/src/assets/Builder.png"
-        title="builder"
-        description="Connects users with local contractors based on their location"
-      />
-      <DesignPageCard
-        img="/src/assets/Blog.png"
-        title="blogr"
-        description="Blogr is a platform for creating an online blog or publication"
-      />
-      <DesignPageCard
-        img="/src/assets/Camp.png"
-        title="camp"
-        description="Get expert training in coding, data, design, and digital marketing"
+        img="/src/assets/science.png"
+        title="science!"
+        description="A poster made in collaboration with the Federal Art Project"
       />
     </div>
   );
