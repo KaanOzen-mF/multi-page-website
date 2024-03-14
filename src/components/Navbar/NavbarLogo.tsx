dd25dd1a1f0bc9ff459a89511d326c317ae25591
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 17:15:27 +0300

    Add navigation functionality to NavbarLogo component

--- src/components/Navbar/NavbarLogo.tsx
+++ src/components/Navbar/NavbarLogo.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const NavbarLogo: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex">
-      <div className="flex">
+      <div
+        className="flex"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         <img className="pr-2 w-full" src="/src/assets/Oval.png" alt="" />
         <p className="font-jost font-bold text-2xl tracking-widest">DESIGNO</p>
       </div>
