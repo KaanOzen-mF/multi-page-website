c77eedd365ad637265c50a0c77a49b7ac9cacbcf
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 16:21:39 +0300

    Add new design cards to GraphicDesignPageCardContainer

--- src/components/GraphicDesignPage/GraphicDesignPageCardContainer.tsx
+++ src/components/GraphicDesignPage/GraphicDesignPageCardContainer.tsx
@@ -9,6 +9,31 @@ const GraphicDesignPageCardContainer: React.FC = () => {
         title="Exspress"
         description="A multi-carrier shipping website for ecommerce businesses"
       />
+      <DesignPageCard
+        img="/src/assets/Transfer.png"
+        title="transfer"
+        description="Site for low-cost money transfers and sending money within seconds"
+      />
+      <DesignPageCard
+        img="/src/assets/Photon.png"
+        title="photon"
+        description="A state-of-the-art music player with high-resolution audio and DSP effects"
+      />
+      <DesignPageCard
+        img="/src/assets/Builder.png"
+        title="builder"
+        description="Connects users with local contractors based on their location"
+      />
+      <DesignPageCard
+        img="/src/assets/Blog.png"
+        title="blogr"
+        description="Blogr is a platform for creating an online blog or publication"
+      />
+      <DesignPageCard
+        img="/src/assets/Camp.png"
+        title="camp"
+        description="Get expert training in coding, data, design, and digital marketing"
+      />
     </div>
   );
 };
