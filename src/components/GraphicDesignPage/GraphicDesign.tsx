d5dd771832c5b3f1ad658bf8f580e78f054197e5
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 16:17:37 +0300

    Add DesignPageHeader and GraphicDesignPageCardContainer components to GraphicDesign.tsx

--- src/components/GraphicDesignPage/GraphicDesign.tsx
+++ src/components/GraphicDesignPage/GraphicDesign.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import DesignPageHeader from "../DesignComponent/DesignPageHeader";
+import GraphicDesignPageCardContainer from "./GraphicDesignPageCardContainer";
 
 const GraphicDesign: React.FC = () => {
   return (
     <div>
-      <h1>Graphic Design</h1>
+      <DesignPageHeader
+        title="Graphic Design"
+        description="We deliver eye-catching branding materials that are tailored to meet your business objectives."
+      />
+      <GraphicDesignPageCardContainer />
     </div>
   );
 };
