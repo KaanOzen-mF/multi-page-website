5af6c0f2801d8479bf767171e06e0ada1c65925c
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-14 17:02:27 +0300

    Add AppDesignPageCardContainer component

--- src/components/AppDesignPage/AppDesign.tsx
+++ src/components/AppDesignPage/AppDesign.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DesignPageHeader from "../DesignComponent/DesignPageHeader";
+import AppDesignPageCardContainer from "./AppDesignPageCardContainer";
 
 const AppDesign: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ const AppDesign: React.FC = () => {
         title="App Design"
         description="Our mobile designs bring intuitive digital solutions to your customers right at their fingertips."
       />
+      <AppDesignPageCardContainer />
     </>
   );
 };
