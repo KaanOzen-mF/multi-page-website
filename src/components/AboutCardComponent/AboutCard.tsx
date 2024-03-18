7237582962831aa76750260f9e6ef1c071df5409
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-18 17:19:46 +0300

    Refactor AboutCardComponent layout  for tablet version

--- src/components/AboutCardComponent/AboutCard.tsx
+++ src/components/AboutCardComponent/AboutCard.tsx
@@ -12,14 +12,16 @@ const AboutCard: React.FC<AboutCardProps> = ({
   imageSrc,
 }) => {
   return (
-    <div className="flex flex-col justify-center items-center h-64 font-jost p-6 mt-48 mb-48">
+    <div className="flex flex-col md:flex-row justify-center items-center h-64 font-jost p-6 mt-48 mb-48 md:mb-9  md:mt-9">
       <img className="mb-8" src={imageSrc} alt={title} />
-      <h3 className="text-xl font-medium tracking-designCardSpacing  mb-12">
-        {title}
-      </h3>
-      <p className="text-center font-normal leading-relaxed mb-48">
-        {description}
-      </p>
+      <div className="flex flex-col items-center md:items-start md:ml-8">
+        <h3 className="text-xl font-medium tracking-designCardSpacing  mb-12 md:mb-4">
+          {title}
+        </h3>
+        <p className="text-center font-normal leading-relaxed mb-48 md:mb-0 md:text-start">
+          {description}
+        </p>
+      </div>
     </div>
   );
 };
