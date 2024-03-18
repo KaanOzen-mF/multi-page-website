ec9f4b2ecbf25ca54e5993ec4c474681dfb9aee2
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-18 17:20:38 +0300

    Refactor DesignPageHeader component for tablet version

--- src/components/DesignComponent/DesignPageHeader.tsx
+++ src/components/DesignComponent/DesignPageHeader.tsx
@@ -10,9 +10,17 @@ const DesignPageHeader: React.FC<DesignPageHeaderProps> = ({
   description,
 }) => {
   return (
-    <div className="flex flex-col justify-center p-6 shadow-md h-80 text-center font-jost bg-[url('/src/assets/bg-pattern-design-pages-intro-mobile.svg')] bg-right-top bg-peach">
-      <h2 className="text-3xl text-white font-semibold mb-4">{title}</h2>
-      <p className="text-white  leading-6 text-sm">{description}</p>
+    <div
+      className="flex flex-col justify-center p-6 shadow-md h-80 text-center font-jost bg-[url('/src/assets/bg-pattern-design-pages-intro-mobile.svg')] bg-right-top bg-peach
+    md:bg-[url('/src/assets/tablet/bg-pattern-design-pages-intro-tablet.svg')] md:bg-right-top md:p-24
+    "
+    >
+      <h2 className="text-3xl md:text-5xl text-white font-medium leading-10 mb-4">
+        {title}
+      </h2>
+      <p className="text-white leading-6 text-sm md:font-normal md:px-32">
+        {description}
+      </p>
     </div>
   );
 };
