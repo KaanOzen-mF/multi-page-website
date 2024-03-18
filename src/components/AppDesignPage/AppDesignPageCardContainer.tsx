3709645ff66fe3e3e70d3674f8221ae09b5db6da
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-18 17:20:04 +0300

    Update image paths in AppDesignPageCardContainer  for tablet version

--- src/components/AppDesignPage/AppDesignPageCardContainer.tsx
+++ src/components/AppDesignPage/AppDesignPageCardContainer.tsx
@@ -5,27 +5,27 @@ const AppDesignPageCardContainer: React.FC = () => {
   return (
     <>
       <DesignPageCard
-        img="/src/assets/image-airfilter.jpg"
+        img="/src/assets/appDesign/image-airfilter.jpg"
         title="airfilter"
         description="Solving the problem of poor indoor air quality by filtering the air"
       />
       <DesignPageCard
-        img="/src/assets/image-eyecam.jpg"
+        img="/src/assets/appDesign/image-eyecam.jpg"
         title="eyecam"
         description="Product that lets you edit your favorite photos and videos at any time"
       />
       <DesignPageCard
-        img="/src/assets/image-faceit.jpg"
+        img="/src/assets/appDesign/image-faceit.jpg"
         title="faceit"
         description="Get to meet your favorite internet superstar with the faceit app"
       />
       <DesignPageCard
-        img="/src/assets/image-todo.jpg"
+        img="/src/assets/appDesign/image-todo.jpg"
         title="todo"
         description="A todo app that features cloud sync with light and dark mode"
       />
       <DesignPageCard
-        img="/src/assets/image-loopstudios.jpg"
+        img="/src/assets/appDesign/image-loopstudios.jpg"
         title="loopstudios"
         description="A VR experience app made for Loopstudios"
       />
