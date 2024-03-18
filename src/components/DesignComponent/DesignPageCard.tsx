26fc230f4e2819e8f5e4873c1fc8dfc05fce9415
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-18 17:20:28 +0300

    Refactor DesignPageCard component layout for tablet version

--- src/components/DesignComponent/DesignPageCard.tsx
+++ src/components/DesignComponent/DesignPageCard.tsx
@@ -12,13 +12,17 @@ const DesignPageCard: React.FC<DesignPageCardProps> = ({
   description,
 }) => {
   return (
-    <div className="font-jost p-6">
-      <img className="rounded-t-xl shadow-md" src={img} alt={title} />
-      <div className="bg-lightPeach rounded-b-xl shadow-md p-6 text-center">
+    <div className="font-jost m-6 sm:rounded-b-xl sm:rounded-t-xl md:rounded-l-xl md:rounded-r-xl  md:flex md:items-center md:justify-center bg-lightPeachWhite">
+      <img
+        className="sm:rounded-t-xl md:rounded-l-xl shadow-md w-full md:w-1/2"
+        src={img}
+        alt={title}
+      />
+      <div className="bg-lightPeachWhite sm:rounded-b-xl md:rounded-r-lg shadow-md text-center">
         <h3 className="font-medium tracking-designCardSpacing text-peach uppercase text-xl leading-relaxed">
           {title}
         </h3>
-        <p className="text-black text-base font-normal leading-relaxed">
+        <p className="text-black text-base font-normal leading-relaxed px-4">
           {description}
         </p>
       </div>
