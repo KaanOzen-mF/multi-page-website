fa61388c5273fa09f9cb126b7e90e249feea1334
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-19 14:54:47 +0300

    Update Navbar padding and logo position for desktop version

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import NavbarLogo from "./NavbarLogo";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex flex-row justify-between items-center h-24 bg-orange-50 p-2 md:px-6">
+    <nav className="flex flex-row justify-between items-center h-24 bg-orange-50 p-2 md:px-6 lg:px-48 lg:pt-12">
       <NavbarLogo />
       <HamburgerBtn />
     </nav>
