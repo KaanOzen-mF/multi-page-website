6a6b9b98ccffbe28efd3359061430c20c0d29daa
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-19 14:53:11 +0300

    Update Footer component styles for desktop version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,27 +10,30 @@ import Button from "./Button";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex flex-col items-center bg-black font-jost text-white px-6 py-12 mt-48">
+    <footer className="flex flex-col items-center bg-black font-jost text-white px-6 py-12 mt-48 lg:px-32">
       <div
         className="flex flex-col items-center justify-center p-12 md:p-12 rounded-xl bg-peach  max-w-lg md:max-w-full w-full text-center -mt-48 mb-6 
-      md:bg-[url('/src/assets/tablet/bg-pattern-design-pages-intro-tablet.svg')] md:bg-right "
+      md:bg-[url('/src/assets/tablet/bg-pattern-design-pages-intro-tablet.svg')] md:bg-right lg:w-mapWidth lg:flex-row lg:justify-between"
       >
-        <h3 className="text-4xl font-medium leading-tight mb-6">
-          Let’s talk about
-          <br />
-          your project
-        </h3>
-        <p className="mb-6">
-          Ready to take it to the next level? Contact us today and find out how
-          our expertise can help your business grow.
-        </p>
+        <div className="lg:flex lg:flex-col lg:text-start">
+          <h3 className="text-4xl font-medium leading-tight mb-6">
+            Let’s talk about
+            <br />
+            your project
+          </h3>
+          <p className="mb-6">
+            Ready to take it to the next level? Contact us today and find out
+            how our expertise can help your business grow.
+          </p>
+        </div>
+
         <Button
           children="GET IN TOUCH"
           className="bg-white text-black font-medium py-2 px-4 rounded-md shadow-lg"
         />
       </div>
 
-      <div className="flex flex-col items-center md:flex-row md:justify-between w-full md:items-center">
+      <div className="flex flex-col items-center md:flex-row md:justify-between w-full md:items-center lg:justify-between lg:w-mapWidth">
         <div className="mt-8 mb-6">
           <div className="flex items-center space-x-2">
             <img src="/src/assets/Oval.png" alt="" className="w-8 h-8" />
@@ -49,9 +52,9 @@ const Footer: React.FC = () => {
 
       <div className="hidden md:block w-full bg-white bg-opacity-10 h-1 my-8" />
 
-      <div className="md:flex md:flex-row md:w-full md:justify-between">
+      <div className="md:flex md:flex-row md:w-full md:justify-between lg:w-mapWidth lg:justify-between">
         <div className="flex flex-col md:flex-row items-center mt-6 text-center">
-          <div className="md:mr-24">
+          <div className="md:mr-24 lg:mr-96">
             <p className="font-bold mb-2">Designo Central Office</p>
             <p className="mb-1">3886 Wellington Street</p>
             <p className="mb-4">Toronto, Ontario M9C 3J5</p>
