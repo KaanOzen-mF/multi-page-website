88d51fc0b894bf3c33a8e9065eca694db591dac9
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-19 14:54:37 +0300

    Refactor Navbar HamburgerBtn component for desktop version

--- src/components/Navbar/HamburgerBtn.tsx
+++ src/components/Navbar/HamburgerBtn.tsx
@@ -28,68 +28,67 @@ const HamburgerBtn: React.FC = () => {
   }, [isOpen]);
 
   return (
-    <>
-      <div className="flex flex-row justify-center items-center" ref={node}>
-        <button
-          onClick={handleClick}
-          className="focus:outline-none lg:hidden md:hidden"
-          aria-label="Menu"
-          aria-expanded={isOpen}
-        >
-          <span
-            className={`bg-black block transition-all duration-300 ease-out 
-                  h-0.5 w-4 rounded-sm ${
+    <div ref={node} className="relative">
+      <button
+        onClick={handleClick}
+        className="focus:outline-none sm:flex md:hidden"
+        aria-label="Menu"
+        aria-expanded={isOpen}
+      >
+        <span
+          className={`bg-black block transition-all duration-300 ease-out 
+                  h-1 w-4 rounded-sm ${
                     isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
                   }`}
-          ></span>
-          <span
-            className={`bg-black block transition-all duration-300 ease-out 
+        ></span>
+        <span
+          className={`bg-black block transition-all duration-300 ease-out 
                   h-0.5 w-4 rounded-sm my-0.5 ${
                     isOpen ? "opacity-0" : "opacity-100"
                   }`}
-          ></span>
-          <span
-            className={`bg-black block transition-all duration-300 ease-out 
+        ></span>
+        <span
+          className={`bg-black block transition-all duration-300 ease-out 
                   h-0.5 w-4 rounded-sm ${
                     isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
                   }`}
-          ></span>
-        </button>
-        {isOpen && (
-          <div className="fixed inset-0 top-[14%] bg-black bg-opacity-60 z-1000 lg:hidden md:hidden">
-            <div className="bg-black text-white p-5 ">
-              <nav className="text-2xl font-jost font-normal">
-                <ul>
-                  <li className="mb-8">
-                    <Link to="/our-company">OUR COMPANY</Link>
-                  </li>
-                  <li className="mb-8">
-                    <Link to="/locations">LOCATIONS</Link>
-                  </li>
-                  <li className="">
-                    <Link to="/contact">CONTACT</Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        )}
+        ></span>
+      </button>
 
-        <nav className="hidden md:flex font-jost font-normal text-2xl md:text-sm md:self-end">
-          <ul className="md:flex md:flex-row items-center justify-around">
-            <li className="mb-8 md:mb-0">
-              <Link to="/our-company">OUR COMPANY</Link>
-            </li>
-            <li className="mb-8 md:mb-0 md:ml-8">
-              <Link to="/locations">LOCATIONS</Link>
-            </li>
-            <li className="md:ml-8">
-              <Link to="/contact">CONTACT</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+      {isOpen && (
+        <div className="fixed inset-0 top-full bg-black bg-opacity-60 z-50">
+          <div className="bg-black text-white p-5 ">
+            <nav className="text-2xl font-jost font-normal">
+              <ul>
+                <li className="mb-8">
+                  <Link to="/our-company">OUR COMPANY</Link>
+                </li>
+                <li className="mb-8">
+                  <Link to="/locations">LOCATIONS</Link>
+                </li>
+                <li className="">
+                  <Link to="/contact">CONTACT</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      )}
+
+      <nav className="hidden sm:hidden md:flex">
+        <ul className="md:flex md:flex-row items-center justify-around">
+          <li className="mb-8 md:mb-0">
+            <Link to="/our-company">OUR COMPANY</Link>
+          </li>
+          <li className="mb-8 md:mb-0 md:ml-8">
+            <Link to="/locations">LOCATIONS</Link>
+          </li>
+          <li className="md:ml-8">
+            <Link to="/contact">CONTACT</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
