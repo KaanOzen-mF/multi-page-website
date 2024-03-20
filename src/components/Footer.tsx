2c85a2e79c9e873e312959971441159b64120bcf
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-20 13:37:16 +0300

    Refactor Footer component styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ import Button from "./Button";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex flex-col items-center bg-black font-jost text-white px-6 py-12 mt-48 lg:px-32">
+    <footer className="flex flex-col bg-black font-jost text-white px-6 py-12 mt-48 lg:pl-40 lg:pr-16">
       <div
         className="flex flex-col items-center justify-center p-12 md:p-12 rounded-xl bg-peach  max-w-lg md:max-w-full w-full text-center -mt-48 mb-6 
       md:bg-[url('/src/assets/tablet/bg-pattern-design-pages-intro-tablet.svg')] md:bg-right lg:w-mapWidth lg:flex-row lg:justify-between"
@@ -50,16 +50,16 @@ const Footer: React.FC = () => {
         </ul>
       </div>
 
-      <div className="hidden md:block w-full bg-white bg-opacity-10 h-1 my-8" />
+      <div className="hidden md:flex w-full bg-white bg-opacity-10 h-1 my-8 " />
 
       <div className="md:flex md:flex-row md:w-full md:justify-between lg:w-mapWidth lg:justify-between">
         <div className="flex flex-col md:flex-row items-center mt-6 text-center">
-          <div className="md:mr-24 lg:mr-96">
+          <div className="md:mr-24 lg:mr-96  md:text-start">
             <p className="font-bold mb-2">Designo Central Office</p>
             <p className="mb-1">3886 Wellington Street</p>
             <p className="mb-4">Toronto, Ontario M9C 3J5</p>
           </div>
-          <div>
+          <div className=" md:text-start">
             <p className="font-bold mb-2">Contact Us (Central Office)</p>
             <p className="mb-1">P : +1 253-863-8967</p>
             <p className="mb-4">M : contact@designo.co</p>
