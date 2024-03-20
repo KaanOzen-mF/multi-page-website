137b2241223457471166329b688da239adc4d89b
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-20 10:15:20 +0300

    Refactor design card layout in WebDesign component

--- src/components/WebDesignPage/WebDesign.tsx
+++ src/components/WebDesignPage/WebDesign.tsx
@@ -13,20 +13,24 @@ const WebDesign: React.FC = () => {
         description="We build websites that serve as powerful marketing tools and bring memorable brand experiences."
       />
       <WebDesignPageCardContainer />
-      <DesignCard
-        title="APP DESIGN"
-        backgroundImage="/src/assets/image-app-design.jpg"
-        onProjectClick={() => {
-          navigate("/app-design"); // Navigate to app design page
-        }}
-      />
-      <DesignCard
-        title="GRAPHIC DESIGN"
-        backgroundImage="/src/assets/image-graphic-design.jpg"
-        onProjectClick={() => {
-          navigate("/graphic-design"); // Navigate to grapic design page
-        }}
-      />
+      <div className="lg:flex lg:flex-row lg:mx-40 lg:w-full">
+        <DesignCard
+          title="APP DESIGN"
+          backgroundImage="/src/assets/image-app-design.jpg"
+          onProjectClick={() => {
+            navigate("/app-design"); // Navigate to app design page
+          }}
+          className="lg:w-half"
+        />
+        <DesignCard
+          title="GRAPHIC DESIGN"
+          backgroundImage="/src/assets/image-graphic-design.jpg"
+          onProjectClick={() => {
+            navigate("/graphic-design"); // Navigate to grapic design page
+          }}
+          className="lg:w-half"
+        />
+      </div>
     </>
   );
 };
