23577928319999957e37e172fe3deecdc95db0c4
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-20 10:14:48 +0300

    Refactor DesignPageCard component styling

--- src/components/DesignComponent/DesignPageCard.tsx
+++ src/components/DesignComponent/DesignPageCard.tsx
@@ -12,17 +12,18 @@ const DesignPageCard: React.FC<DesignPageCardProps> = ({
   description,
 }) => {
   return (
-    <div className="font-jost m-6 sm:rounded-b-xl sm:rounded-t-xl md:rounded-l-xl md:rounded-r-xl  md:flex md:items-center md:justify-center bg-lightPeachWhite">
+    <div className="group font-jost m-6 sm:rounded-b-xl sm:rounded-t-xl md:rounded-l-xl md:rounded-r-xl md:flex md:items-center md:justify-center bg-lightPeachWhite lg:flex lg:flex-col lg:bg-white lg:w-upperQuarter lg:rounded-xl shadow-md transition duration-300 ease-in-out hover:bg-peach">
       <img
-        className="sm:rounded-t-xl md:rounded-l-xl shadow-md w-full md:w-1/2"
+        className="sm:rounded-t-xl md:rounded-l-xl lg:rounded-t-xl rounded-b-none w-full md:w-1/2 lg:w-full object-cover"
         src={img}
         alt={title}
       />
-      <div className="bg-lightPeachWhite sm:rounded-b-xl md:rounded-r-lg shadow-md text-center">
-        <h3 className="font-medium tracking-designCardSpacing text-peach uppercase text-xl leading-relaxed">
+
+      <div className="text-center p-6 flex flex-col justify-between sm:rounded-b-xl md:rounded-r-lg lg:w-full lg:rounded-b-xl transition duration-300 ease-in-out">
+        <h3 className="font-medium tracking-designCardSpacing text-peach uppercase text-xl leading-relaxed transition duration-300 ease-in-out group-hover:text-white">
           {title}
         </h3>
-        <p className="text-black text-base font-normal leading-relaxed px-4">
+        <p className="text-black text-base font-normal leading-relaxed transition duration-300 ease-in-out group-hover:text-white">
           {description}
         </p>
       </div>
