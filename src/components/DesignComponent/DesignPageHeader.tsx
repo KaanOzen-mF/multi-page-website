c412be93d8b82a55ef2c04abb5fc02d337517465
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-20 10:14:55 +0300

    Refactor DesignPageHeader component styles

--- src/components/DesignComponent/DesignPageHeader.tsx
+++ src/components/DesignComponent/DesignPageHeader.tsx
@@ -13,6 +13,7 @@ const DesignPageHeader: React.FC<DesignPageHeaderProps> = ({
     <div
       className="flex flex-col justify-center p-6 shadow-md h-80 text-center font-jost bg-[url('/src/assets/bg-pattern-design-pages-intro-mobile.svg')] bg-right-top bg-peach
     md:bg-[url('/src/assets/tablet/bg-pattern-design-pages-intro-tablet.svg')] md:bg-right-top md:p-24
+    lg:mx-48 lg:mt-6 lg:rounded-2xl
     "
     >
       <h2 className="text-3xl md:text-5xl text-white font-medium leading-10 mb-4">
