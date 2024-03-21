d43125723fb78e2d933fd2cb7439f9d3ccec34e0
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-21 10:22:51 +0300

    comment line add

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import CountryCards from "../CountryCards";
 import ContactForm from "./ContactForm";
 
+// The ContactPage component serves as the main container for the contact section of the website.
 const ContactPage: React.FC = () => {
   return (
     <>
+      {/* ContactForm component is embedded to provide users with a form to submit their inquiries. */}
       <ContactForm />
+
+      {/* Container for country cards displaying office locations. It adjusts layout based on screen size. */}
       <div className="flex flex-col lg:flex-row lg:justify-between lg:mx-96">
+        {/* CountryCards component instances for each location. Each card is passed a country name and an image filename as props. */}
         <CountryCards country="canada" image="canada.svg" />
         <CountryCards country="australia" image="australia.svg" />
         <CountryCards country="united Kingdom" image="unitedKingdom.svg" />
