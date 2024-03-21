cd1eda4ffc8c39441f62068e96fc6829197b9eb4
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-21 10:48:55 +0300

    comment line add

--- src/components/GraphicDesignPage/GraphicDesign.tsx
+++ src/components/GraphicDesignPage/GraphicDesign.tsx
@@ -4,31 +4,39 @@ import GraphicDesignPageCardContainer from "./GraphicDesignPageCardContainer";
 import DesignCard from "../DesignComponent/DesignCard";
 import { useNavigate } from "react-router-dom";
 
+// The GraphicDesign component serves as the main page for showcasing graphic design projects.
 const GraphicDesign: React.FC = () => {
-  const navigate = useNavigate(); // Initialize useHistory
+  const navigate = useNavigate(); // Hook for programmatic navigation
+
   return (
     <div>
+      {/* DesignPageHeader component displaying the title and description for the graphic design page */}
       <DesignPageHeader
         title="Graphic Design"
         description="We deliver eye-catching branding materials that are tailored to meet your business objectives."
       />
 
+      {/* Container for graphic design project cards */}
       <GraphicDesignPageCardContainer />
 
+      {/* Additional design categories (App and Web Design) represented as clickable cards */}
       <div className="lg:flex lg:flex-row lg:mx-40 lg:w-full lg:mt-24">
+        {/* DesignCard for App Design. Clicking this card navigates to the App Design page. */}
         <DesignCard
           title="APP DESIGN"
           backgroundImage="/src/assets/image-app-design.jpg"
           onProjectClick={() => {
-            navigate("/app-design"); // Navigate to app design page
+            navigate("/app-design"); // Function to navigate to the app design page
           }}
           className="lg:w-half"
         />
+
+        {/* DesignCard for Web Design. Clicking this card navigates to the Web Design page. */}
         <DesignCard
           title="WEB DESIGN"
           backgroundImage="/src/assets/image-web-design.jpg"
           onProjectClick={() => {
-            navigate("/web-design"); // Navigate to web design page
+            navigate("/web-design"); // Function to navigate to the web design page
           }}
           className="lg:w-half"
         />
--- src/components/GraphicDesignPage/GraphicDesignPageCardContainer.tsx
+++ src/components/GraphicDesignPage/GraphicDesignPageCardContainer.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 import DesignPageCard from "../DesignComponent/DesignPageCard";
 
+// The GraphicDesignPageCardContainer component is responsible for laying out the individual project cards related to graphic design.
 const GraphicDesignPageCardContainer: React.FC = () => {
   return (
+    // Container for the design cards, using a grid layout on larger screens for a cohesive look.
     <div className="lg:grid lg:grid-cols-3 lg:gap-4 lg:ml-40 lg:mr-20">
+      {/* DesignPageCard for the 'TIM BROWN' project */}
       <DesignPageCard
-        img="/src/assets/graphicDesign/image-change.jpg"
-        title="TIM BROWN"
-        description="A book cover designed for 
-        Tim Brown’s new release, ‘Change’"
+        img="/src/assets/graphicDesign/image-change.jpg" // Image source for the card background
+        title="TIM BROWN" // Title of the project
+        description="A book cover designed for Tim Brown’s new release, ‘Change’" // Short description of the project
       />
 
+      {/* DesignPageCard for the 'Boxed Water' project */}
       <DesignPageCard
-        img="/src/assets/graphicDesign/image-boxed-water.jpg"
-        title="Boxed Water"
-        description="A simple packaging concept made for Boxed Water"
+        img="/src/assets/graphicDesign/image-boxed-water.jpg" // Image source for the card background
+        title="Boxed Water" // Title of the project
+        description="A simple packaging concept made for Boxed Water" // Short description of the project
       />
+
+      {/* DesignPageCard for the 'science!' project */}
       <DesignPageCard
-        img="/src/assets/graphicDesign/image-science.jpg"
-        title="science!"
-        description="A poster made in collaboration with the Federal Art Project"
+        img="/src/assets/graphicDesign/image-science.jpg" // Image source for the card background
+        title="science!" // Title of the project
+        description="A poster made in collaboration with the Federal Art Project" // Short description of the project
       />
     </div>
   );
