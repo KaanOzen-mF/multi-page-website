0bdbceb73ce7c18553965689fb9f0fb9608adc19
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-22 10:48:17 +0300

    img path change

--- src/components/HomePage/AboutCardContainer.tsx
+++ src/components/HomePage/AboutCardContainer.tsx
@@ -11,21 +11,21 @@ const AboutCardContainer: React.FC = () => {
       <AboutCard
         title="PASSIONATE"
         description="Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions."
-        imageSrc="/static/aboutCard/passionate.svg" // Path to the image that visually represents passion
+        imageSrc="/aboutCard/passionate.svg" // Path to the image that visually represents passion
       />
 
       {/* AboutCard component for "RESOURCEFUL". It highlights the strategic purpose behind every project and the agile approach for superior results. */}
       <AboutCard
         title="RESOURCEFUL"
         description="Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs."
-        imageSrc="/static/aboutCard/resourceful.svg" // Path to the image that visually represents resourcefulness
+        imageSrc="/aboutCard/resourceful.svg" // Path to the image that visually represents resourcefulness
       />
 
       {/* AboutCard component for "FRIENDLY". It emphasizes the company's focus on customer satisfaction and providing the best possible experience. */}
       <AboutCard
         title="FRIENDLY"
         description="We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide."
-        imageSrc="/static/aboutCard/friendly.svg" // Path to the image that visually represents friendliness
+        imageSrc="/aboutCard/friendly.svg" // Path to the image that visually represents friendliness
       />
     </div>
   );
