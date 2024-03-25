7d1b340856fc5755bae113b6a30a3e0298c4bbee
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-25 17:02:36 +0300

    Add form validation and error handling to ContactForm component

--- src/components/ContactPage/ContactForm.tsx
+++ src/components/ContactPage/ContactForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "../Button";
+import { FaExclamationCircle } from "react-icons/fa";
 
 // The ContactForm component manages the state of form inputs and handles their submission.
 const ContactForm: React.FC = () => {
@@ -10,18 +11,54 @@ const ContactForm: React.FC = () => {
     phone: "",
     message: "",
   });
+  //State to store the form errors
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const validateForm = () => {
+    let errors = { name: "", email: "", message: "" };
+    let formIsValid = true;
+
+    // İsim alanı boş mu kontrol edin
+    if (!formData.name.trim()) {
+      errors.name = "Can't be empty";
+      formIsValid = false;
+    }
+
+    // E-posta adresi geçerli mi kontrol edin
+    if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Please use a valid email address";
+      formIsValid = false;
+    }
+
+    // Mesaj alanı boş mu kontrol edin
+    if (!formData.message.trim()) {
+      errors.message = "Can't be empty";
+      formIsValid = false;
+    }
+
+    setFormErrors(errors);
+    return formIsValid;
+  };
 
   // Function to update the state when a form input changes.
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormErrors({ ...formErrors, [e.target.name]: "" });
   };
 
   // Function to handle the form submission.
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Implementation for what happens on form submit goes here (e.g., sending data to an API).
+    if (validateForm()) {
+      // Eğer form geçerliyse, form gönderme işlemini burada yapın
+      console.log("Form is valid and can be submitted");
+    }
   };
 
   return (
@@ -55,8 +92,17 @@ const ContactForm: React.FC = () => {
           value={formData.name}
           onChange={handleChange}
           placeholder="Name"
-          className="w-full bg-transparent border-b border-white placeholder-white px-3 py-5 focus:border-b-2 focus:border-white focus:outline-none focus:font-medium"
+          className="w-full bg-transparent border-b border-white placeholder-white px-3 py-5 focus:border-b-2 focus:border-white 
+          focus:outline-none focus:font-medium relative block"
         />
+
+        {formErrors.name && (
+          <div className="absolute flex items-center text-center text-lightPeachWhite right-formNotfication top-nameTop italic text-sm md:text-l md:top-nameTopMd lg:top-nameTopLg lg:right-formNotficationLg">
+            <FaExclamationCircle />
+            <span className="ml-1">{formErrors.name}</span>
+          </div>
+        )}
+
         <input
           type="email"
           id="email"
@@ -66,6 +112,12 @@ const ContactForm: React.FC = () => {
           placeholder="Email Address"
           className="w-full bg-transparent border-b border-white px-3 py-5  placeholder-white focus:border-b-2 focus:border-white focus:outline-none focus:font-medium"
         />
+        {formErrors.email && (
+          <div className="absolute flex items-center text-center text-lightPeachWhite right-formNotfication top-emailTop italic text-sm md:text-l md:top-emailTopMd lg:top-emailTopLg lg:right-formNotficationLg">
+            <FaExclamationCircle />
+            <span className="ml-1">{formErrors.email}</span>
+          </div>
+        )}
         <input
           type="phone"
           id="phone"
@@ -83,6 +135,12 @@ const ContactForm: React.FC = () => {
           placeholder="Your Message"
           className="w-full bg-transparent border-b border-white px-3 py-5  placeholder-white focus:border-b-2 focus:border-white focus:outline-none focus:font-medium"
         />
+        {formErrors.message && (
+          <div className="absolute flex items-center text-center text-lightPeachWhite italic right-formNotfication top-messeageTop text-sm md:text-l md:top-messageTopMd lg:top-messageTopLg lg:right-formNotficationLg">
+            <FaExclamationCircle />
+            <span className="ml-1">{formErrors.message}</span>
+          </div>
+        )}
         <Button
           children={"Submit"} // Text displayed on the button
           className="bg-white md:w-1/4 md:self-end mt-6 text-black" // Styling applied to the button
